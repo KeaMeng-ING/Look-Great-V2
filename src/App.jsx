75e7de3dff0deeb0f3435b42c146f9122e2ad3dc
KeaMeng <2023484ing@aupp.edu.kh>
2025-01-05 23:51:25 +0700

    Add Card & Header and Config tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import "./App.css";
 import { useEffect, useState } from "react";
+import Card from "./components/Card";
+import Header from "./components/Header";
 
 function App() {
   const [allProduct, setAllProduct] = useState(null);
@@ -7,7 +9,7 @@ function App() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch("https://fakestoreapi.com/products?limit=5", { mode: "cors" })
+    fetch("https://fakestoreapi.com/products?limit=20", { mode: "cors" })
       .then((response) => {
         if (response.status >= 400) {
           throw new Error("server error");
@@ -15,27 +17,51 @@ function App() {
         return response.json();
       })
       .then((data) => {
-        const productDetails = {
-          id: data.id,
-          title: data.title,
-          price: data.price,
-          rating: data.rating,
-          image: data.image,
-        };
+        console.log(data);
+        const productDetails = data.map((product) => ({
+          id: product.id,
+          title: product.title,
+          price: product.price,
+          rating: product.rating.rate,
+          count: product.rating.count,
+          image: product.image,
+        }));
         setAllProduct(productDetails);
       })
       .catch((error) => setError(error))
       .finally(() => setLoading(false));
-  });
+  }, []);
 
-  if (loading) return <p>Loading...</p>;
+  if (loading) {
+    return (
+      <>
+        <Header />
+        <div className="loading-container">
+          <span className="loader"></span>
+        </div>
+      </>
+    );
+  }
+
   if (error) return <p>A network error was encountered</p>;
 
-  console.log(allProduct);
   return (
     <>
-      <h1>H213i</h1>
-      {/* <img src={allProduct[0].image} alt={"placeholder text"} /> */}
+      <Header />
+
+      <div className="mb-4 grid gap-4 sm:grid-cols-2 md:mb-8 lg:grid-cols-3 xl:grid-cols-4 mx-24 mt-4">
+        {allProduct.map((product) => (
+          <Card key={product.id} {...product} />
+        ))}
+      </div>
+      <div className="w-full text-center">
+        <button
+          type="button"
+          className="rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-100  "
+        >
+          Show more
+        </button>
+      </div>
     </>
   );
 }
