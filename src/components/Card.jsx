62a5ab5f084c6361637b7168259bef2c5e1b8050
KeaMeng <2023484ing@aupp.edu.kh>
2025-01-24 15:02:09 +0700

    ResponsiveUpdated

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -198,6 +198,15 @@ export default function Card({
 
             <Link
               to="/cart"
+              state={{
+                title,
+                price,
+                rating,
+                count,
+                image,
+                description,
+                quantity: 1,
+              }}
               className="inline-flex items-center rounded-lg bg-primary-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-primary-800 focus:outline-none focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 whitespace-nowrap"
             >
               <svg
